4111673bc8f00821f12994e67ee600b7cf225cbb
Aaron0Chillz <charlesunlimited10@gmail.com>
2023-06-07 06:20:35 -0700

    updated the code for pycodestyle

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
+""" prints the titles of the first 10 hot posts for a given subreddit.
 """
 
 import requests
@@ -13,12 +12,14 @@ def top_ten(subreddit):
     Args:
         subreddit (str): The name of the subreddit.
     """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Aaron0Chillz"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
         data = response.json()
         posts = data["data"]["children"]
+
         for i in range(10):
             if i < len(posts):
                 title = posts[i]["data"]["title"]
