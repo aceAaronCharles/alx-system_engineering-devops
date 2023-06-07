83e7f88c6c515dd3fc02be73bd6ab70b277005c0
Aaron0Chillz <charlesunlimited10@gmail.com>
2023-06-07 06:32:45 -0700

    updated the code for pycodestyle

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,20 +13,21 @@ def top_ten(subreddit):
     Args:
         subreddit (str): The name of the subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
         "(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
+    response = requests.get(
+        url, headers=headers, allow_redirects=False
+    )
     if response.status_code == 200:
         data = response.json()
         posts = data["data"]["children"]
-
         for i in range(10):
             if i < len(posts):
                 title = posts[i]["data"]["title"]
                 print(title)
     else:
         print(None)
+
